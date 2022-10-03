6908ab753995fa89cd1d435d85da329a26b01718
ShindanoSingi <shindanosingi@sekur.com>
2022-10-03 03:19:37 -0500

    c4

---
+++ build/precache-manifest.6efa308faf19a35ce90a8eed2b855092.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0d7ba3622245bd2875ffa43f106306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeba368b16ce88a412eb",
+    "url": "/static/css/main.6e5544d7.chunk.css"
+  },
+  {
+    "revision": "f1aa2db68279478dc154",
+    "url": "/static/js/2.75f1ea23.chunk.js"
+  },
+  {
+    "revision": "cda8f586e4be294fe855a04c470227df",
+    "url": "/static/js/2.75f1ea23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeba368b16ce88a412eb",
+    "url": "/static/js/main.27344c5b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ffb95c65c38fe62400d007bf49ee419a",
+    "url": "/static/media/HOL.ffb95c65.png"
+  },
+  {
+    "revision": "84d0375a9a40383399be90cf890f17bc",
+    "url": "/static/media/Trivia.84d0375a.png"
+  },
+  {
+    "revision": "1a7aef2b7b5eac7fca8ff64bbc1de9ee",
+    "url": "/static/media/avatar.1a7aef2b.png"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "6e0a60964de7640feae2c68394948aac",
+    "url": "/static/media/cmcc.6e0a6096.png"
+  },
+  {
+    "revision": "e954c1810043388e1957fdd7ae840751",
+    "url": "/static/media/coffee-apple.e954c181.jpeg"
+  },
+  {
+    "revision": "dd63c22d4082acf71b4cfc2324ec1961",
+    "url": "/static/media/css.dd63c22d.png"
+  },
+  {
+    "revision": "52e903cdbf52b11b73e9fcb55ef99981",
+    "url": "/static/media/django.52e903cd.svg"
+  },
+  {
+    "revision": "e8a25052d6ee0a19cd066e42c41543bf",
+    "url": "/static/media/express.e8a25052.svg"
+  },
+  {
+    "revision": "8980c1b1a00fbd94ab7259d19812db7c",
+    "url": "/static/media/github.8980c1b1.svg"
+  },
+  {
+    "revision": "55f31ab66a0d9e1565e8a7aec3f00a1f",
+    "url": "/static/media/heroku.55f31ab6.svg"
+  },
+  {
+    "revision": "d63db51fd8a050478b2c5b5136cb94a3",
+    "url": "/static/media/html.d63db51f.svg"
+  },
+  {
+    "revision": "12bac607c472dfee8483d057e8515ec2",
+    "url": "/static/media/javascript.12bac607.png"
+  },
+  {
+    "revision": "689c91b98bd6bebb4162fd08cf445fa4",
+    "url": "/static/media/jquery.689c91b9.jpeg"
+  },
+  {
+    "revision": "6654c90fe70f8f6e037ca267e4b29178",
+    "url": "/static/media/material-ui.6654c90f.svg"
+  },
+  {
+    "revision": "41f5757f0bf2bda69efba04c27dcd98e",
+    "url": "/static/media/materialize.41f5757f.svg"
+  },
+  {
+    "revision": "411cbd3a9561f89cbc5f5023fcbc6ad8",
+    "url": "/static/media/mobile.411cbd3a.gif"
+  },
+  {
+    "revision": "7b8eceb8096fb7043ab8787a0cb166fd",
+    "url": "/static/media/mongodb.7b8eceb8.svg"
+  },
+  {
+    "revision": "a765b97becafaa65d3df9c72a12cae62",
+    "url": "/static/media/msgc.a765b97b.jpeg"
+  },
+  {
+    "revision": "445d41318a0fb0a0990d4bee3eb9dc9c",
+    "url": "/static/media/mysql.445d4131.svg"
+  },
+  {
+    "revision": "63153a47fff276431d7b721ba8d0ae8f",
+    "url": "/static/media/netlify.63153a47.svg"
+  },
+  {
+    "revision": "9a3d5fd0effbf61a34242f14928c1242",
+    "url": "/static/media/node js.9a3d5fd0.webp"
+  },
+  {
+    "revision": "20147627b6ca6569111f38681acfd63f",
+    "url": "/static/media/pdf.20147627.pdf"
+  },
+  {
+    "revision": "3df930dfd92a3d0729be0f9cb01c49d1",
+    "url": "/static/media/react.3df930df.svg"
+  },
+  {
+    "revision": "fa2ad26523c0e22f15c08d4f0ba7c800",
+    "url": "/static/media/sql.fa2ad265.svg"
+  },
+  {
+    "revision": "56f85180f4c81a7888ad1606fdb0d1e7",
+    "url": "/static/media/sql1.56f85180.svg"
+  },
+  {
+    "revision": "23c785efddbe0f6cb04ba6ce72d539a3",
+    "url": "/static/media/userM.23c785ef.png"
+  },
+  {
+    "revision": "a0212b2863faf8d844cd2c616c41f09c",
+    "url": "/static/media/usm.a0212b28.png"
+  },
+  {
+    "revision": "1fe8b30dd4ec04535fc5240dc9d5689e",
+    "url": "/static/media/weather.1fe8b30d.gif"
+  },
+  {
+    "revision": "f57306b5ff72692a8e7f1921bd328721",
+    "url": "/static/media/youtube.f57306b5.gif"
+  }
+]);
