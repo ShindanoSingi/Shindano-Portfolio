32c0298ce2d5c41c5d64c403790b069648b4bbb5
ShindanoSingi <shindanosingi@sekur.com>
2022-10-04 22:51:34 -0400

    c5

---
+++ .history/src/components/Portfolio_20220101052819.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import {FaHandPointUp} from "react-icons/fa";
+import './Portfolio.css'
+
+const Portfolio = () => {
+     const [showTopBtn, setShowTopBtn] = useState(false);
+     useEffect(() => {
+          window.addEventListener("scroll", () => {
+               if (window.scrollY > 400) {
+                    setShowTopBtn(true);
+               } else {
+                    setShowTopBtn(false);
+               }
+          });
+     }, []);
+     const goToTop = () => {
+          window.scrollTo({top: 0, behavior: "smooth"});
+     };
+     return (
+          <div>
+               <div className=" top-to-btm">
+                    {
+                    showTopBtn && (
+                         <FaHandPointUp className="icon-position icon-style"
+                              onClick={goToTop}/>
+                    )
+               } </div>
+          </div>
+     );
+};
+export default Portfolio;
